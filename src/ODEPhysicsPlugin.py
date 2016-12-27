352bedc5a7dcf69537e80ad636fbe8527fa96e85
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-12-27 17:24:44 +0100

    Make things work reasonably well for object collision

--- src/ODEPhysicsPlugin.py
+++ src/ODEPhysicsPlugin.py
@@ -19,19 +19,28 @@ class ODEPhysicsPlugin(Extension):
         JobQueue.getInstance().jobFinished.connect(self._onJobFinished)
 
         self._world = ode.World()
-        self._world.setGravity( (0, toODE(-9810), 0) )
+        self._world.setGravity( (0, toODE(-98100), 0) )
         self._world.setERP(0.2)
         self._world.setCFM(1e-5)
 
-        self._world.setContactMaxCorrectingVel(toODE(10000))
-        self._world.setContactSurfaceLayer(0.0001)
+        #self._world.setContactMaxCorrectingVel(toODE(1000))
+        self._world.setContactSurfaceLayer(0.001)
+
+        self._world.setLinearDamping(1)
+        #self._world.setAngularDamping(0)
+
+        self._world.setAutoDisableLinearThreshold(0.001)
+        #self._world.setAutoDisableTime(0.1)
 
         self._space = ode.Space(space_type = 1)
 
         self._contact_group = ode.JointGroup()
 
         self._contact_bounce = 0.0
-        self._contact_friction = float("inf")
+        #self._contact_friction = float("inf")
+        self._contact_friction = 0
+
+        self._initial_positioning = False
 
         self._build_plate = ode.GeomPlane(self._space, (0, 1, 0), 0)
 
@@ -51,9 +60,40 @@ class ODEPhysicsPlugin(Extension):
         if not nodes:
             return
 
+        fixed_nodes = []
+        for node in DepthFirstIterator(Application.getInstance().getController().getScene().getRoot()):
+            if node.hasDecoration("setFixed"):
+                fixed_nodes.append(node)
+                node.callDecoration("setFixed", True)
+
         for node in nodes:
-            node.translate(Vector(0, 100, 0))
             node.addDecorator(RigidBodyDecorator(self._world, self._space))
+
+        #self._world.setLinearDamping(0)
+        #self._contact_bounce = 1
+        #self._world.setContactMaxCorrectingVel(toODE(1000))
+        self._initial_positioning = True
+
+        for i in range(1000):
+            self._space.collide( (self._world, self._contact_group), self._onCollision )
+            self._world.quickStep( (self._update_interval / 1000) / self._steps_per_update )
+            self._contact_group.empty()
+
+            #for node in nodes:
+                #geom = node.callDecoration("getGeom")
+                #if geom and hasattr(geom, "collision_count"):
+
+                    #force = node.getWorldPosition().normalized() * (geom.collision_count * 10000000)
+                    #node.callDecoration("getBody").addForce( (force.x, force.y, force.z) )
+
+        #self._world.setLinearDamping(1)
+        #self._world.setContactMaxCorrectingVel(float("inf"))
+        #self._contact_bounce = 0
+
+        self._initial_positioning = False
+
+        #for node in fixed_nodes:
+            #node.callDecoration("setFixed", False)
 
     def _onCollision(self, args, geom1, geom2):
         contacts = ode.collide(geom1, geom2)
@@ -64,6 +104,13 @@ class ODEPhysicsPlugin(Extension):
             c.setMu(self._contact_friction)
             j = ode.ContactJoint(world, contact_group, c)
             j.attach(geom1.getBody(), geom2.getBody())
+
+            #if self._initial_positioning:
+                #for geom in (geom1, geom2):
+                    #if geom.getBody() and not geom.getBody().isKinematic():
+                        #if not hasattr(geom, "collision_count"):
+                            #geom.collision_count = 0
+                        #geom.collision_count += 1
 
     def _onTimer(self):
         dt = self._update_interval / 1000
--- src/RigidBodyDecorator.py
+++ src/RigidBodyDecorator.py
@@ -6,10 +6,11 @@ from UM.Scene.SceneNode import SceneNode
 from UM.Math.Vector import Vector
 from UM.Math.Quaternion import Quaternion
 from UM.Math.Color import Color
+from UM.Math.Matrix import Matrix
 
 from UM.Mesh.MeshBuilder import MeshBuilder
 
-from .Helpers import ScaleFactor, fromODE, toODE
+from . import Helpers
 
 class RigidBodyDecorator(SceneNodeDecorator):
     def __init__(self, world, space):
@@ -21,6 +22,8 @@ class RigidBodyDecorator(SceneNodeDecorator):
         self._geom = None
         self._update_from_ode = False
 
+        self._fixed = False
+
     def setNode(self, node):
         super().setNode(node)
 
@@ -30,14 +33,17 @@ class RigidBodyDecorator(SceneNodeDecorator):
 
         if not self._body:
             self._body = ode.Body(self._world)
+            self._body.setMaxAngularSpeed(0)
             mass = ode.Mass()
-            mass.setBox(5.0, toODE(aabb.width), toODE(aabb.height), toODE(aabb.depth))
+            mass.setBox(5.0, Helpers.toODE(aabb.width), Helpers.toODE(aabb.height), Helpers.toODE(aabb.depth))
             self._body.setMass(mass)
 
 
         if not self._geom:
             if node.getMeshData():
-                mesh = node.getMeshData()
+                scale_matrix = Matrix()
+                scale_matrix.setByScaleFactor(1.01)
+                mesh = node.getMeshData().getTransformed(scale_matrix)
 
                 self._trimesh = ode.TriMeshData()
 
@@ -51,7 +57,7 @@ class RigidBodyDecorator(SceneNodeDecorator):
 
                 self._fixWindingOrder(vertices, indices, debug_builder)
 
-                self._trimesh.build(vertices / ScaleFactor, indices)
+                self._trimesh.build(vertices / Helpers.ScaleFactor, indices)
 
                 self._geom = ode.GeomTriMesh(self._trimesh, self._space)
 
@@ -60,9 +66,9 @@ class RigidBodyDecorator(SceneNodeDecorator):
                 for i in range(self._geom.getTriangleCount()):
                     tri = self._geom.getTriangle(i)
 
-                    v0 = fromODE(tri[0])
-                    v1 = fromODE(tri[1])
-                    v2 = fromODE(tri[2])
+                    v0 = Helpers.fromODE(tri[0])
+                    v1 = Helpers.fromODE(tri[1])
+                    v2 = Helpers.fromODE(tri[2])
 
                     #normal = calculateNormal(v0, v1, v2)
 
@@ -87,78 +93,79 @@ class RigidBodyDecorator(SceneNodeDecorator):
                     return True
                 n.render = _renderNormals
             else:
-                self._geom = ode.GeomBox(self._space, lengths = (toODE(aabb.width), toODE(aabb.height), toODE(aabb.depth)))
+                self._geom = ode.GeomBox(self._space, lengths = (Helpers.toODE(aabb.width), Helpers.toODE(aabb.height), Helpers.toODE(aabb.depth)))
 
             self._geom.setBody(self._body)
 
-        self._body.setPosition(toODE(node.getWorldPosition()))
+        self._body.setPosition(Helpers.toODE(node.getWorldPosition()))
 
         node.transformationChanged.connect(self._onTransformationChanged)
+
+    def getBody(self):
+        return self._body
+
+    def getGeom(self):
+        return self._geom
 
     def updateFromODE(self):
         self._update_from_ode = True
 
-        self.getNode().setPosition(fromODE(self._body.getPosition()), SceneNode.TransformSpace.World)
+        self.getNode().setPosition(Helpers.fromODE(self._body.getPosition()), SceneNode.TransformSpace.World)
 
         body_orientation = self._body.getQuaternion()
         self.getNode().setOrientation(Quaternion(body_orientation[3], body_orientation[0], body_orientation[1], body_orientation[2]))
 
         self._update_from_ode = False
 
+    def setFixed(self, fixed):
+        if fixed != self._fixed:
+            self._fixed = fixed
+
+            self._body.setGravityMode(not self._fixed)
+
+            if self._fixed:
+                self._body.setKinematic()
+            else:
+                self._body.setDynamic()
+
+    def toODE(self, value):
+        return Helpers.toODE(value)
+
     def _onTransformationChanged(self, node):
         if self._update_from_ode:
             return
 
-        self._body.setPosition(toODE(self.getNode().getWorldPosition()))
+        self._body.setPosition(Helpers.toODE(self.getNode().getWorldPosition()))
 
     def _fixWindingOrder(self, vertices, indices, debug_builder):
-        print(indices)
         for i in range(len(indices)):
             v0 = vertices[indices[i][0]]
             v1 = vertices[indices[i][1]]
             v2 = vertices[indices[i][2]]
 
-            print("v0", v0)
-            print("v1", v1)
-            print("v2", v2)
-
             normal = calculateNormal(v0, v1, v2)
 
-            #debug_builder.addLine(Vector(data = v0), Vector(data = v0 + normal * 10), Color(0.0, 1.0, 0.0, 1.0))
-            #debug_builder.addLine(Vector(data = v1), Vector(data = v1 + normal * 10), Color(0.0, 1.0, 0.0, 1.0))
-            #debug_builder.addLine(Vector(data = v2), Vector(data = v2 + normal * 10), Color(0.0, 1.0, 0.0, 1.0))
-
-            print("normal", normal)
-
             center = (v0 + v1 + v2) / 3
-
-            print("center", center)
 
             direction = center / numpy.linalg.norm(center)
 
             debug_builder.addLine(Vector(data = center), Vector(data = center + direction * 10), Color(0.0, 0.0, 1.0, 1.0))
             debug_builder.addLine(Vector(data = center), Vector(data = center + normal * 10), Color(0.0, 1.0, 0.0, 1.0))
 
-            print("direction", direction)
-            print("dot", normal.dot(direction))
-
-            if normal.dot(direction) < 0.5:
-                print("flipping winding order of index entry", i)
+            if normal.dot(direction) < 0.1:
                 indices[i][0], indices[i][2] = indices[i][2], indices[i][0]
                 new_normal = calculateNormal(v2, v1, v0)
                 debug_builder.addLine(Vector(data = center), Vector(data = center + new_normal * 10), Color(1.0, 0.0, 0.0, 1.0))
-
-
-
-
-        print(indices)
 
 def calculateNormal(v0, v1, v2):
     edge0 = v0 - v1
     edge1 = v0 - v2
 
     normal = numpy.cross(edge0, edge1)
-    normal = normal / numpy.linalg.norm(normal)
+    length = numpy.linalg.norm(normal)
+
+    if length > 0:
+        normal = normal / length
 
     return normal
 
